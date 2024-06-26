6c10483d8c62d61c95c746b79817204551aacfd9
pirateIV <benabolade@gmail.com>
2024-06-26 11:27:40 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 const { Server } = require("socket.io");
-
 const app = require("./app");
 const Message = require("./models/Message");
-const authMiddleware = require("./middleware/authMiddleware");
-
 const port = 5000;
 
+// Start the Express server
 const expressServer = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// Initialize Socket.io
 const io = new Server(expressServer, {
   cors: {
     origin: "*",
   },
 });
 
+// Store the Socket.io instance in the app object
 app.set("io", io);
 
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
-  // Upon connection - only to user
+  // Welcome message to the connected user
   socket.emit("message", "Welcome to chat");
 
   // Broadcast when a user connects
@@ -34,10 +34,19 @@ io.on("connection", (socket) => {
     console.log(msg);
   });
 
+  // Handle sending messages
   socket.on("send_message", async (message) => {
-    const newMessage = new Message(message);
-    await newMessage.save();
-    console.log(newMessage);
-    io.to(message.receiver).emit("recieve_message", message);
+    try {
+      const newMessage = new Message(message);
+      await newMessage.save();
+      io.emit("receive_message", message);
+    } catch (error) {
+      console.error("Error saving message:", error);
+    }
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
   });
 });
